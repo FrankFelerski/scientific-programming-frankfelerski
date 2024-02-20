ea9462560be6013824a14225f2cf0a8ef9d5ee3e
FrankFelerski <98779199+FrankFelerski@users.noreply.github.com>
2024-02-20 15:19:05 -0600

    Update calculate_liter_mass.py

--- computing_with_formulas/calculate_liter_mass.py
+++ computing_with_formulas/calculate_liter_mass.py
@@ -1,3 +1,3 @@
-initmass = float(input('Mass per liters (g)? '))
+initmass = float(input('Mass per liters (g)? ')) #because I don't have the list this program allows you to input initial conditions
 finalmass = initmass/1000
 print(finalmass)
