bf4315a9b5d7c8d228362501aea342389cfc2df4
FrankFelerski <98779199+FrankFelerski@users.noreply.github.com>
2024-02-20 15:14:12 -0600

    Update convert_length.py

---
+++ computing_with_formulas/convert_length.py
@@ -0,0 +1,7 @@
+lengthinmeters = float(input('length in meters? '))
+meterstocm = lengthinmeters * 100
+inchesincm = meterstocm / 2.54
+feetininches = inchesincm / 12
+yardsinfeet = feetininches / 3
+milesinyards = yardsinfeet / 1760
+print(milesinyards)
