824c99841d054d8a745c5493d85e2794f02eca90
FrankFelerski <98779199+FrankFelerski@users.noreply.github.com>
2024-02-20 15:09:19 -0600

    Update compare_life_expectancy_to_seconds.py

---
+++ computing_with_formulas/compare_life_expectancy_to_seconds.py
@@ -0,0 +1,7 @@
+yearsinlife = 100
+daysinyear = 365.25
+hoursinday = 24
+minutesinhour = 60
+secondsinminute = 60
+secondsinlife = daysinyear * hoursinday * minutesinhour * secondsinminute * yearsinlife
+print(secondsinlife)
